50cb73cc1dbde2a2793f6c16d0ce2b001ebc0004
Stella <stellahyhuang@hotmail.com>
2019-01-28 23:22:35 -0800

    initial attempt at templating the 4 main functions

--- vector_template.cpp
+++ vector_template.cpp
@@ -49,13 +49,15 @@ namespace Pic10c{
 
 // 4 main functions modified to accept type T objects
 
-vector::vector()
-: the_data(nullptr), size(0), capacity(INIT_CAP) {
+template <typename T>
+vector<T>::vector()
+: data(nullptr), size(0), capacity(INIT_CAP) {
     
     data = new T[capacity];
 }
 
-vector::vector(const vector& source)
+template <typename T>
+vector<T>::vector(const vector& source)
 : data(nullptr), size(source.size), capacity(source.capacity) {
     
     data = new T[capacity];
@@ -65,6 +67,67 @@ vector::vector(const vector& source)
         data[i] = source.data[i];
     }
 }
+
+template <typename T>
+vector<T>&::operator=(const vector& rhs) {
+    if (this != &rhs) {     // Self-assignment?
+        // Release old memory and request more
+        delete[] data;
+        data = new T[rhs.capacity];
+        
+        // Shallow copy non-pointers
+        size = rhs.size;
+        capacity = rhs.capacity;
+        
+        // Deep copy internal array
+        for (int i = 0 ; i < the_size ; ++i)
+            data[i] = rhs.data[i];
+    }
+    return *this;
+}
+
+
+template <typename T>
+vector<T>::~vector(){
+    delete[] data;
+}
+
+
+
+//////////////////// Other functions //////////////////////
+
+bool vector::empty() const {
+    return (size == 0);
+}
+
+size_t vector::size() const {
+    return size;
+}
+
+size_t vector::capacity() const {
+    return capacity;
+}
+
+template <typename T>
+T vector::front() const {
+    return *data;
+}
+
+template <typename T>
+T vector::back() const {
+    return *(the_data + the_size - 1);
+}
+
+template <typename A_Type>
+T vector::at( size_t index ) const {
+    if (index < size)
+        return data[index];
+    return data[0];
+}
+
+
+
+
 
 
 
